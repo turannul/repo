51d17e0da27861eadae77479d0fde4663c596d6f
turannul-mobile(filza) <turanull000@gmail.com>
2022-02-06 07:00:03 +0300

    Something is new?

--- commit.sh
+++ commit.sh
@@ -2,7 +2,7 @@
 # Filza da kullanmak için, Terminali normal (nonSuperuser) olarak #ayarlayın.
 cd /private/var/mobile/Documents/Github/repo
 git pull
-./var/mobile/Documents/Github/repo/updaterepo.sh
+/var/mobile/Documents/Github/repo/updaterepo.sh
 git add /private/var/mobile/Documents/Github/repo
 git commit -a -m "Something is new?" 
 git push
