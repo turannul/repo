f3aef2ec74170ea8ed6af78c4927b6ceccdfc6f8
iPhone-NewTerm <nonameconfirned7@gmail.com>
2022-03-25 09:48:29 +0300

    Updated.

--- scripts/Sign/unloadSignature.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-sudo apt-key del 085B6CCD035FF7906623E04AE59C6432536DF4E2
-
-echo "ಥ﹏ಥ"
-echo "ಥ﹏ಥ"
-echo "ಥ﹏ಥ"
-echo "ಥ﹏ಥ"
-echo "ಥ﹏ಥ"
--- scripts/update.sh
+++
@@ -1,6 +0,0 @@
-#! /opt/procursus/bin/bash
-cd /Users/turannul/Documents/GitHub/repo
-chmod +x sign.sh
-chmod +x updaterepo.sh
-./updaterepo.sh
-./sign.sh
