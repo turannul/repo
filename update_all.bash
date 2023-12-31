e030f95aaab4a9ff2b22f690a3d8d250af8c196c
Turann_ <84424530+turannul@users.noreply.github.com>
2023-12-31 11:46:06 +0300

    .

--- update_all.bash
+++ update_all.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-macOS_update_repo(){
+brew_check(){
     echo "Checking for Homebrew, wget, xz, & zstd..."
     if test ! "$(which brew)"; then
         /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
@@ -8,47 +8,80 @@ macOS_update_repo(){
     brew list --verbose wget || brew install wget
     brew list --verbose xz || brew install xz
     brew list --verbose zstd || brew install zstd
+    clear
+    echo "Homebrew checked."
+}
+
+apt_ftparchive(){
     echo "Downloading apt-ftparchive..."
     if [ ! -e "apt-ftparchive" ]; then
         wget -q -nc https://apt.procurs.us/apt-ftparchive && sudo chmod 751 ./apt-ftparchive
     fi
+    echo "apt-ftparchive downloaded."
+}
+
+update_rootless(){
+    echo "Updating (rootless) repository..."
+    cd rootless/ || exit
     rm Packages Packages.xz Packages.gz Packages.bz2 Packages.zst Release 2> /dev/null
-    rm rootless/Packages rootless/Packages.xz rootless/Packages.gz rootless/Packages.bz2 rootless/Packages.zst rootless/Release 2> /dev/null
-    echo "Updating repository..."
-    ./apt-ftparchive packages ./deb_files > Packages
-    ./apt-ftparchive packages ./deb_files_rootless > rootless/Packages
-    ./apt-ftparchive release -c ./repo/repo.conf . > Release
-    ./apt-ftparchive release -c ./rootless/repo/repo.conf . > rootless/Release
-    echo "Repository Update successful, compressing files..."
+    apt-ftparchive packages ./deb_files_rootless > Packages
     gzip -c9 Packages > Packages.gz
-    gzip -c9 rootless/Packages > rootless/Packages.gz
     xz -c9 Packages > Packages.xz
-    xz -c9 rootless/Packages > rootless/Packages.xz
     bzip2 -c9 Packages > Packages.bz2
-    bzip2 -c9 rootless/Packages > rootless/Packages.bz2
     zstd -c19 Packages > Packages.zst
-    zstd -c19 rootless/Packages > rootless/Packages.zst 
+    apt-ftparchive release -c ./config/rootless.conf . > Release
+}
+
+update_rootful(){
+    echo "Updating (rootful) repository..."
+    cd "$(dirname "$0")" || exit
+    rm Packages Packages.xz Packages.gz Packages.bz2 Packages.zst Release 2> /dev/null
+    ./apt-ftparchive packages ./deb_files > Packages
+    gzip -c9 Packages > Packages.gz
+    xz -c9 Packages > Packages.xz
+    bzip2 -c9 Packages > Packages.bz2
+    zstd -c19 Packages > Packages.zst
+    apt-ftparchive release -c ./config/rootful.conf . > Release
+}
+
+
+update_rootless_macOS(){
+    echo "Updating (rootless) repository..."
+    cd rootless/ || exit
+    rm Packages Packages.xz Packages.gz Packages.bz2 Packages.zst Release 2> /dev/null
+    ./apt-ftparchive packages ../deb_files_rootless > Packages
+    gzip -c9 Packages > Packages.gz
+    xz -c9 Packages > Packages.xz
+    bzip2 -c9 Packages > Packages.bz2
+    zstd -c19 Packages > Packages.zst
+    ./apt-ftparchive release -c ./config/rootless.conf . > Release
+}
+
+update_rootful_macOS(){
+    echo "Updating (rootful) repository..."
+    cd "$(dirname "$0")" || exit
+    rm Packages Packages.xz Packages.gz Packages.bz2 Packages.zst Release 2> /dev/null
+    ./apt-ftparchive packages ./deb_files > Packages
+    gzip -c9 Packages > Packages.gz
+    xz -c9 Packages > Packages.xz
+    bzip2 -c9 Packages > Packages.bz2
+    zstd -c19 Packages > Packages.zst
+    ./apt-ftparchive release -c ./config/rootful.conf . > Release
+}
+
+macOS_update_repo(){
+    brew_check
+    apt_ftparchive
+    echo "Update in progress..."
+    update_rootful_macOS
+    update_rootless_macOS
     echo "Repository Updated."
 }
 
 linux_update_repo(){
-    echo "Updating repository..."
-    rm Packages Packages.xz Packages.gz Packages.bz2 Packages.zst Release 2> /dev/null
-    rm rootless/Packages rootless/Packages.xz rootless/Packages.gz rootless/Packages.bz2 rootless/Packages.zst rootless/Release 2> /dev/null
-
-    apt-ftparchive packages ./deb_files > Packages
-    apt-ftparchive packages ./deb_files_rootless > rootless/Packages
-    apt-ftparchive release -c ./repo/repo.conf . > Release
-    apt-ftparchive release -c ./rootless/repo/repo.conf . > rootless/Release
-
-    gzip -c9 Packages > Packages.gz
-    gzip -c9 rootless/Packages > rootless/Packages.gz
-    xz -c9 Packages > Packages.xz
-    xz -c9 rootless/Packages > rootless/Packages.xz
-    bzip2 -c9 Packages > Packages.bz2
-    bzip2 -c9 rootless/Packages > rootless/Packages.bz2
-    zstd -c19 Packages > Packages.zst
-    zstd -c19 rootless/Packages > rootless/Packages.zst
+    echo "Update in progress..."
+    update_rootful
+    update_rootless
     echo "Repository Updated."
 }
 
